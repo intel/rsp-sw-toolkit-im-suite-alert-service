d6536e4fab2001600f2dd292a40779610d9dabb7
Daniel Villanueva <daniel.a.villanueva@intel.com>
2017-09-19 11:14:42 -0700

    update Handlers to return default page

---
+++ app/routes/handlers/handlers.go
@@ -0,0 +1,38 @@
+/*
+ * INTEL CONFIDENTIAL
+ * Copyright (2017) Intel Corporation.
+ *
+ * The source code contained or described herein and all documents related to the source code ("Material")
+ * are owned by Intel Corporation or its suppliers or licensors. Title to the Material remains with
+ * Intel Corporation or its suppliers and licensors. The Material may contain trade secrets and proprietary
+ * and confidential information of Intel Corporation and its suppliers and licensors, and is protected by
+ * worldwide copyright and trade secret laws and treaty provisions. No part of the Material may be used,
+ * copied, reproduced, modified, published, uploaded, posted, transmitted, distributed, or disclosed in
+ * any way without Intel/'s prior express written permission.
+ * No license under any patent, copyright, trade secret or other intellectual property right is granted
+ * to or conferred upon you by disclosure or delivery of the Materials, either expressly, by implication,
+ * inducement, estoppel or otherwise. Any license under such intellectual property rights must be express
+ * and approved by Intel in writing.
+ * Unless otherwise agreed by Intel in writing, you may not remove or alter this notice or any other
+ * notice embedded in Materials by Intel or Intel's suppliers or licensors in any way.
+ */
+
+package handlers
+
+import (
+	"context"
+	"net/http"
+
+	"github.impcloud.net/Responsive-Retail-MVP/rfid-alert-service/pkg/web"
+)
+
+// Alerts represents the User API method handler set.
+type Alerts struct {
+}
+
+// GetIndex retrieves the index file
+// 200 OK
+func (alerts *Alerts) GetIndex(ctx context.Context, writer http.ResponseWriter, request *http.Request) error {
+	web.RespondHTML(writer, "RFID Alert Service", "RFID Alert Service is running", http.StatusOK)
+	return nil
+}
