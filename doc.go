77599e07b4992c5f211dd8929326761470d9aa90
siva radhakrishnan <siva.radhakrishnan@intel.com>
2018-02-21 17:29:01 -0700

    WIP- Added doc.go and update README

---
+++ doc.go
@@ -0,0 +1,57 @@
+// RFID Alert Service API
+//
+// RFID Alert service provides the capabilities to monitor gateway status and alerts. Gateway status is updated periodically based on events sent through Heartbeat listener from CS SDK and posted to the configured REST endpoint in configuration.json.
+// In a similar way, alert events are processed and posted to the respective configured REST endpoint.
+//
+//  __Configuration Values__
+// <blockquote>RFID Alert service configuration is split between values set in a configuration file and those set as environment values in compose file. The configuration file is expected to be contained in a docker secret for production deployments, but can be on a docker volume for validation and development.
+// <blockquote><b>Configuration file values</b>
+// <blockquote>•<b> serviceName</b> - Runtime name of the service.</blockquote>
+// <blockquote>•<b> loggingLevel</b> - Logging level to use: "info" (default) or "debug" (verbose).</blockquote>
+// <blockquote>•<b> port</b> - Port to run the service's HTTP Server on.</blockquote>
+// <blockquote>•<b> watchdogMinutes</b> - Time interval set to check the status of registered gateways</blockquote>
+// <blockquote>•<b> maxMissedHeartbeats</b> - Maximum heart beats that a gateway can miss before getting deregistered.</blockquote>
+// <blockquote>•<b> sendHeartbeatTo</b> - Endpoint to send the processed heart beat.</blockquote>
+// <blockquote>•<b> sendAlertTo</b> - Endpoint to send the processed alert.</blockquote>
+// <blockquote>•<b> contextSensing</b> - Host and port number for the Context Broker.</blockquote>
+// </blockquote>
+// <blockquote><b>Compose file environment variable values</b>
+// <blockquote>•<b> runtimeConfigPath</b> - Path to the configuration file to use at runtime.</blockquote>
+// </blockquote>
+//
+// <pre><b>Example configuration file json
+// &#9{
+// &#9&#9"serviceName": "RRP - RFID-Alert-service",
+// &#9&#9"loggingLevel": "debug",
+// &#9&#9"port": "8080",
+// &#9&#9"watchdogMinutes" : 1,
+// &#9&#9"maxMissedHeartbeats" : 2,
+// &#9&#9"contextSenisng" : "127.0.0.1:8888",
+// &#9&#9"sendHeartBeatTo" : "http://localhost:9005/heartbeat",
+// &#9&#9"sendAlertTo" : "http://localhost:9005/alert",
+// &#9}
+// </b></pre>
+// <pre><b>Example environment variables in compose file
+// &#9runtimeConfigPath: "/data/configs/rfid-alert.json"
+// </b></pre>
+//
+// __Known services this service depends on:__
+// ○ Context Sensing
+//
+// __Known services that depend upon this service:__
+// ○ None
+//
+// Schemes: http, https
+// Host: RFID-Alert-service:8080
+//   Contact: RRP <rrp@intel.com>
+// BasePath: /
+// Version: 0.0.1
+//
+// Consumes:
+// - application/json
+//
+// Produces:
+// - application/json
+//
+// swagger:meta
+package main
